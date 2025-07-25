8918b71f91cbe87a6f1b39b394910c21b0b7d3fb
Clara Giraud <contact.claragiraud@gmail.com>
2025-07-25 17:09:16 +0200

    add sound system

---
+++ Flower City/Assets/05_Script/M_soundSlider.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundSlider
+{
+    public string name;
+    public Slider slider;
+    public AudioSource audioSource;
+    public TextMeshProUGUI volumeText;
+}
+public class M_soundSlider : MonoBehaviour
+{
+    [Header("Liste des sliders de sons")]
+    public SoundSlider[] soundSliders;
+
+    private void Start()
+    {
+        foreach (var sound in soundSliders)
+        {
+            if (sound.slider != null && sound.audioSource != null)
+            {
+                sound.slider.onValueChanged.AddListener((value) => OnSliderChanged(sound));
+                OnSliderChanged(sound); // initialiser volume au départ
+            }
+        }
+    }
+
+    private void OnSliderChanged(SoundSlider sound)
+    {
+        sound.audioSource.volume = sound.slider.value;
+
+        if (sound.volumeText != null)
+        {
+            int percentage = Mathf.RoundToInt(sound.slider.value * 100f);
+            sound.volumeText.text = percentage + " %";
+        }
+    }
+}
